63ff99d83abe1fdc07e6f9c7e1f7955d6858f8be
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2021-03-24 20:24:58 -0400

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ app.get('/', (req, res) => {
     console.log('I have recieved the current time')
 });
 
+// Fetch Current Weather By City Name
 app.get('/weather/:cityName', (req, res) => {
     let {cityName} = req.params;
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${weatherApiKey}`
@@ -27,7 +28,50 @@ app.get('/weather/:cityName', (req, res) => {
         res.send(data)
     })
     .catch(err => { console.log(err); });  
-})
+});
+
+//Fetch Current Weather By Zip
+app.get('/weather/:zipcode', (req, res) => {
+    let {zipcode, countrycode} = req.params;
+    let url = `http://api.openweathermap.org/data/2.5/weather?zip=${zipcode},${countrycode}&appid=${weatherApiKey}`;
+
+    fetch(url)
+    .then(response => {return response.json()})
+    .then(data => {
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.send(data)
+    })
+    .catch(err => { console.log(err); });  
+});
+
+// Fetch 5 Day Forecasts By Name
+app.get('/forecast/:cityname', (req, res) => {
+    let {cityname} = req.params;
+    let url = `http://api.openweathermap.org/data/2.5/forecast?q=${cityname}&appid=${weatherApiKey}`;
+
+    fetch(url)
+    .then(response => {return response.json()})
+    .then(data => {
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.send(data)
+    })
+    .catch(err => { console.log(err); });  
+});
+
+//Fetch 5 Day Forecasts By Zip
+app.get('/forecast/:zipcode', (req, res) => {
+    let {zipcode, countrycode} = req.params;
+    let url = `http://api.openweathermap.org/data/2.5/forecast?zip=${zipcode},${countrycode}&appid=${weatherApiKey}`;
+
+    fetch(url)
+    .then(response => {return response.json()})
+    .then(data => {
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.send(data)
+    })
+    .catch(err => { console.log(err); });  
+
+});
 
 server.listen(PORT); 
 console.log(`Server is listening on ${PORT}`);
