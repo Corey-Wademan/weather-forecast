2995a4600e6a2f3497177110627c0f1dcc3a8bf3
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2021-03-30 11:49:21 -0400

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import WeatherContainer from './Containers/WeatherContainer';
+
+
+
+const App = () => {
+  return (
+    <>
+    <WeatherContainer />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/Components/CurrentWeatherComponent.js
@@ -0,0 +1,12 @@
+// Contains all data for current weather
+import React from 'react'
+
+const CurrentWeatherComponent = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CurrentWeatherComponent
