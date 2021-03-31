4731971f85eb430ff804c29131b47b21897db270
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2021-03-31 19:10:48 -0400

    Add files via upload

---
+++ client/src/Containers/ForecastContainer.js
@@ -0,0 +1,13 @@
+// Contains the forecast components (Hourly & Day)
+import React from 'react'
+
+
+const ForecastContainer = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ForecastContainer
