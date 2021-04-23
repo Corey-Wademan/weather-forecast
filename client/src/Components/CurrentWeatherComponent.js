1ca98ef9302b4370e945ac001513512f55486bc1
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2021-04-22 23:07:54 -0400

    Add files via upload

--- client/src/Components/CurrentWeatherComponent.js
+++ client/src/Components/CurrentWeatherComponent.js
@@ -2,33 +2,34 @@
 import React, {useEffect} from 'react';
 import '../Styles/CurrentWeatherComponent.css';
 
-const CurrentWeatherComponent = ({ currentWeather }) => {
+const CurrentWeatherComponent = ({ currentWeather, location }) => {
     
-    let timestamp = currentWeather.dt
-    let sunriseStamp = currentWeather.sys.sunrise
-    let sunsetStamp = currentWeather.sys.sunset
+    let timestamp = currentWeather.current.dt
+    let sunriseStamp = currentWeather.current.sunrise
+    let sunsetStamp = currentWeather.current.sunset
     
-
+// ADD IN ADDITION FOR SELECTED TIMEZONE
     const stampFormatter = (stamp) => {
         let date = new Date(stamp * 1000);
         let hours = date.getHours();
         let minutes = "0" + date.getMinutes();
         let formattedTime;
+        // let timezone = currentWeather.timezone
         if (hours > 12) {
-            return formattedTime = hours - 12 + ':' + minutes.substr(-2)
+            return formattedTime = hours - 12 + ':' + minutes.substr(-2) + ' p.m.';
         } else 
-        return formattedTime = hours + ':' + minutes.substr(-2);
+        return formattedTime = hours + ':' + minutes.substr(-2) + ' a.m.';
     }
 
     const compassDirection = (degrees) => {
         let x, y, r, ctx, radians;
         ctx = window.compass.getContext("2d");
-        // subtract 90 so that north is up then convert to radians
+        // add 90 so that north is up then convert to radians so arrow points in correct direction
         radians = 0.0174533 * (degrees + 90);
         // calc compass centre 
         x = ctx.canvas.width / 2;
         y = ctx.canvas.height / 2;
-        r = x * 0.3;
+        r = x * 0.37;
         // clear 
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
         ctx.beginPath();
@@ -38,18 +39,21 @@ const CurrentWeatherComponent = ({ currentWeather }) => {
         ctx.lineCap = 'round';
         ctx.shadowOffsetX = 1;
         ctx.shadowOffsetY = 1;
-        ctx.shadowBlur = 5;
+        ctx.shadowBlur = 10;
         ctx.shadowColor = "rgba(0, 0, 0, 0.5)";
         // draw compass needle
         ctx.lineWidth = 7;
         ctx.moveTo(x, y);
         ctx.lineTo(x + r * Math.cos(radians), y + r * Math.sin(radians));
         ctx.stroke();
-    };
+        // arrow 
+    }; 
+
+    
       
 
     useEffect(() => {
-        compassDirection(currentWeather.wind.deg);
+        compassDirection(currentWeather.current.wind_deg);
     })
 
 
@@ -59,22 +63,22 @@ const CurrentWeatherComponent = ({ currentWeather }) => {
     return (
         <div>
             <div className='location-info'>
-        
-
+                <h2>{location} <span>|</span> {stampFormatter(timestamp)}</h2>
             </div>
             <div className='row'>
                 <div className='main'>
-                    <h2>{Math.round(currentWeather.main.temp)} ℉</h2>
-                    <h4>Feels Like: {currentWeather.main.feels_like} ℉</h4>
-                    <h4>{currentWeather.weather[0].description}</h4>
+                    <h2>{Math.round(currentWeather.current.temp)} <span>℉</span></h2>
+                    <h4>Feels Like: {currentWeather.current.feels_like} <span>℉</span></h4>
+                    <h4>{currentWeather.current.weather[0].main}</h4>
                 </div>
                 <div className='sun-settings'>
-                    <h2>Sunrise: {stampFormatter(sunriseStamp)}<span>a.m.</span> | Sunset: {stampFormatter(sunsetStamp)}<span>p.m.</span></h2>
+                    <h2>Sunrise: {stampFormatter(sunriseStamp)} | Sunset: {stampFormatter(sunsetStamp)}</h2>
                 </div>
                 <div className='sub-main'>
-                    <h3>Humidity: {currentWeather.main.humidity}%</h3>
-                    <h3>Wind: {Math.round(currentWeather.wind.speed)}mph</h3>
-                    <canvas id="compass" height='120' width='200' />
+                    <h3>Humidity: {currentWeather.current.humidity}%</h3>
+                    <h3>Wind: {Math.round(currentWeather.current.wind_speed)}mph</h3>
+                    <h3>Wind Direction</h3>
+                    <canvas id="compass" height='130' width='250' />
                 </div>
             </div>
         </div>
