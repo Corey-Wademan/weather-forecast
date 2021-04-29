5d1e27aab1adb736cf5e7cd004d4b4df3921902b
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2021-04-29 10:38:54 -0400

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const PORT = 5000;
 const fetch = require('node-fetch')
 const app = express();
-require('dotenv').config()
+
 const cors = require('cors');
 
 app.use(express.static("client"));
-app.use(cors());
+app.use(cors()); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -14,15 +14,15 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/weather/:searchTerm', (req, res) => {
     const searchTerm = req.params.searchTerm;  
     
-    let latLong = `https://maps.googleapis.com/maps/api/geocode/json?address=${searchTerm}&key=${process.env.googleAPIKEY}`;
-    
+    let latLong = `https://maps.googleapis.com/maps/api/geocode/json?address=${searchTerm}&key=${googleAPIKEY}`;
+     
  
     fetch(latLong)
         .then(res => { return res.json() })
         .then(areaData => {
             lat = areaData.results[0].geometry.location.lat;
             long = areaData.results[0].geometry.location.lng;
-            return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&units=imperial&appid=${process.env.weatherApiKey}`)
+            return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&units=imperial&appid=${weatherApiKey}`)
                 .then(response => { return response.json() })
                 .then(weatherData => {
                     res.send({weatherData, areaData})
@@ -31,6 +31,6 @@ app.get('/weather/:searchTerm', (req, res) => {
 }); 
 
 
-app.listen(PORT); 
+app.listen(process.env.PORT || PORT); 
 console.log(`Server is listening on ${PORT}`);
 
